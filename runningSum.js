a7a761009d5a1ab840baee3003f18c66c0bbb8de
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2023-03-28 01:37:41 +0000

    Completed find-pivot-index leetcode exercise.

--- runningSum.js
+++ runningSum.js
@@ -1,12 +1,21 @@
 /**
  * @param {number[]} nums
- * @return {number[]}
+ * @return {number}
  */
-var runningSum = function (nums) {
+var pivotIndex = function (nums) {
+  let left = 0;
+  let right = 0;
+
   for (let i = 0; i < nums.length; i++) {
-    if (i !== 0) {
-      nums[i] += nums[i - 1];
-    }
+    right += nums[i];
   }
-  return nums
+
+  for (let j = 0; j < nums.length - 1; j++) {
+    right -= nums[j];
+    if (left === right) {
+      return j;
+    }
+    left += nums[j];
+  }
+  return -1;
 };
