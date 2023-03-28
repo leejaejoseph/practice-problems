07898a4624bcf7bb026710b5d701dd6e51d6b42b
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2023-03-28 02:53:35 +0000

    Completed leetcode find pivot index exercise.

---
+++ findPivotIndex.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+  let left = 0;
+  let right = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    right += nums[i];
+  }
+
+  for (let j = 0; j < nums.length - 1; j++) {
+    right -= nums[j];
+    if (left === right) {
+      return j;
+    }
+    left += nums[j];
+  }
+  if (left === 0) {
+    return nums.length - 1;
+  }
+  return -1;
+};
