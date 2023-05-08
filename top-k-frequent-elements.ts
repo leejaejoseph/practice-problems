27ca6d7d3c44b9d559f7108959e0a3d2853027f7
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2023-05-08 08:21:04 +0000

    Completed top-k-frequent-elements.

--- top-k-frequent-elements.ts
+++ top-k-frequent-elements.ts
@@ -1,7 +1,12 @@
 function topKFrequent(nums: number[], k: number): number[] {
-  const list: Record<number, number> = {}// 0 - k
-  const bucketList: Record<number, number[]> = {}// 0 - k
+  const list: Record<number, number> = {};
+  const bucketArray: number[][] = [];
   const returnArray: number[] = [];
+
+  for (let i = 0; i < nums.length + 1; i++) {
+    bucketArray.push([]);
+  }
+
   for (let num of nums) {
     if (!list[num]) {
       list[num] = 0;
@@ -10,57 +15,22 @@ function topKFrequent(nums: number[], k: number): number[] {
   }
 
   for (let key in list) {
-    if (!bucketList[list[key]]) {
-      bucketList[list[key]] = [];
-    }
-    bucketList[list[key]].push(Number(key));
+    bucketArray[list[key]].push(Number(key));
   }
 
-  for (let i: number = nums.length; i > 0; i--) {
-    try {
-      for (let j = 0; j < bucketList[i].length; j++) {
-        if (returnArray.length === k) {
-          return returnArray;
-        }
-        returnArray.push(bucketList[i][j])
-    } catch (e) {
+  for (let i = bucketArray.length - 1; i >= 0; i--) {
+    if (returnArray.length === k) {
+      return returnArray;
+    }
+    if (bucketArray[i].length === 0) {
       continue;
+    } else if (bucketArray[i].length > 1) {
+      for (let j = 0; j < bucketArray[i].length; j++) {
+        returnArray.push(Number(bucketArray[i][j]))
+      }
+    } else {
+      returnArray.push(Number(bucketArray[i]))
     }
   }
   return returnArray;
-};
-
-// 2
-
-  function topKFrequent(nums: number[], k: number): number[] {
-    const list: Record<number, number> = {};
-    const bucketArray = [];
-    const returnArray: number[] = [];
-
-    for (let i = 0; i !== nums.length; i++) {
-      bucketArray.push([]);
-    }
-
-    for (let num of nums) {
-      if (!list[num]) {
-        list[num] = 0;
-      }
-      list[num]++;
-    }
-
-    for (let key in list) {
-      console.log(key, list[key], bucketArray)
-      bucketArray[list[key]].push(Number(key));
-      console.log(key, list[key], bucketArray)
-    }
-
-    for (let i = bucketArray.length - 1; i >= 0; i--) {
-      if (returnArray)
-        if (bucketArray[i].length === 0) {
-          continue;
-        }
-      console.log(bucketArray[i], bucketArray, Number(bucketArray[i]))
-    }
-    return returnArray;
-
-  }
+}
