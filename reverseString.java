7673220909b5b8007093bf30dfbeb4c6f4c5f9a4
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2023-05-13 00:50:02 +0000

    Completed reverseString exercise.

--- reverseString.java
+++ reverseString.java
@@ -1,5 +1,5 @@
-class Solution {
-    public char[] reverseString(char[] s) {
+class reverseString {
+    public void inPlace(char[] s) { //Time complexity is O(n) as it goes through every variable but is 0(1) as it does not use extra data structures.
         int left = 0;
         int right = s.length - 1;
 
@@ -11,5 +11,24 @@ class Solution {
             right--;
         }
         return s;
+    }
+    public void lastIndex(char[] s) { //Time complexity is O(n + n) as it goes through every variable but is 0(n) as it recreates every reverse indexed char in new char[].
+      char[] returnArray = new char[s.length];
+      for (int i = 0, j = s.length - 1; j > -1; i++, j--) {
+          returnArray[i] = s[j];
+      }
+      for (int i = 0; i < returnArray.length; i++) {
+          s[i] = returnArray[i];
+      }
+    }
+
+    public void usingArrayList(char[] s) {
+        ArrayList<Character> returnArray = new ArrayList<Character>();
+        for (int i = 0, j = s.length - 1; j > -1; i++, j--) {
+            returnArray.add(s[j]);
+        }
+        for (int i = 0; i < returnArray.size(); i++) {
+            s[i] = returnArray.get(i);
+        }
     }
 }
