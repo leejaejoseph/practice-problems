40e655cafe4410d82d358a2ab3a4a1f521d22ea2
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2023-05-26 00:54:06 +0000

    Revised using XOR Swap to reduce temp.

--- sortArrayByParity.java
+++ sortArrayByParity.java
@@ -3,12 +3,12 @@ class Solution {
         int l = 0;
         int r = nums.length - 1;
         while(l < r) {
-            int temp = nums[l];
             if (nums[l] % 2 == 0) {
                 l++;
             } else if (nums[r] % 2 == 0) {
-                nums[l] = nums[r];
-                nums[r] = temp;
+                nums[l] ^= nums[r];
+                nums[r] ^= nums[l];
+                nums[l] ^= nums[r];
                 l++;
                 r--;
             } else {
