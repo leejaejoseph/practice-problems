8c433c53e0e676a3014f53bced83ddc55bf4b819
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2023-06-08 07:34:52 +0000

    Completed LeetCode 61.

--- rotateList.java
+++ rotateList.java
@@ -35,3 +35,42 @@ class Solution {
         return head;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) return null;
+
+        ListNode curr = head;
+        int len = 1;
+        while (curr.next != null) {
+            len++;
+            curr = curr.next;
+        }
+        curr.next = head;
+
+        k = len - k % len;
+
+        while (k > 0) {
+            curr = curr.next;
+            k--;
+        }
+        head = curr.next;
+        while (len > 0) {
+            curr = curr.next;
+            len--;
+        }
+        curr.next = null;
+        return head;
+    }
+}
