a0dea44af788e66cfd76f2be08d07d3c55e33b92
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2023-06-10 07:07:25 +0000

    Corrected Attempt.

--- diagonalTraverse.java
+++ diagonalTraverse.java
@@ -36,3 +36,48 @@ class Solution {
         return returning;
     }
 }
+
+
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        ArrayList<Integer> returning = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> nums = new ArrayList<>();
+        ArrayList<Integer> diagonal;
+        int row = 0;
+        int x = 0;
+        int y = 0;
+        int decreasing = 0;
+
+        for (int i = 0; i < mat.length * 2 - 1; i++){
+            diagonal = new ArrayList<>();
+            while (x >= 0 && y < mat[0].length){
+                diagonal.add(mat[x][y]);
+                x--;
+                y++;
+            }
+            if (row < mat.length - 1) {
+                row++;
+                x = row;
+                y = 0;
+            } else {
+                decreasing++;
+                x = mat.length - 1;
+                y = decreasing;
+            }
+            nums.add(diagonal);
+        }
+
+        for (int j = 0; j < nums.size(); j++){
+            if (j % 2 != 0) {
+                Collections.reverse(nums.get(j));
+            }
+            returning.addAll(nums.get(j));
+        }
+
+        int[] res = new int[returning.size()];
+        for (int i = 0; i < returning.size(); i++) {
+            res[i] = returning.get(i);
+        }
+        return res;
+    }
+}
